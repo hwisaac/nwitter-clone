62fad3b48f2f5bdb9123739b545c159395f7236f
hwisaac <54179672+hwisaac@users.noreply.github.com>
2022-12-27 03:57:21 +0900

    Updates

---
+++ precache-manifest.9d36c1af4ad3b0e33b6ce6d20e783713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820ad1a2712c9f5fefc0107ff4e82830",
+    "url": "/nwitter-clone/index.html"
+  },
+  {
+    "revision": "6e84638635fee95ec9bd",
+    "url": "/nwitter-clone/static/css/main.c0c71231.chunk.css"
+  },
+  {
+    "revision": "e56176d16b19ddaa338e",
+    "url": "/nwitter-clone/static/js/2.fdd71235.chunk.js"
+  },
+  {
+    "revision": "f996b13041dd5e3ac13d9051d815f701",
+    "url": "/nwitter-clone/static/js/2.fdd71235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e84638635fee95ec9bd",
+    "url": "/nwitter-clone/static/js/main.dced1ad6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/nwitter-clone/static/js/main.dced1ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fc31a64bea47c11a34",
+    "url": "/nwitter-clone/static/js/runtime-main.c94cf636.js"
+  }
+]);
